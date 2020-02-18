399015d5ae5477fbdd23415f6ac00012c1d0529c
Nilo Rocha <nilsr44@gmail.com>
2020-02-18 16:31:48 +0100

    ex001

--- fattoriale.js
+++ fattoriale.js
@@ -1,3 +1,11 @@
 /**
 * Scrivere qui il codice della funzione fattoriale(n)
 **/
+const fatorial = function(n){
+    for(let x = n-1; x>=1; x--){
+        n = n * x
+    }    
+    return n
+}
+let result = fatorial(4)
+console.log(result)
